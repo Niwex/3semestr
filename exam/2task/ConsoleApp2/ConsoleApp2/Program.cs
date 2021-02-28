a537653c57cefded48c697f5859bd2f93cb36205
Daniil <chelpanov98@gmail.com>
2021-02-28 19:11:46 +0700

    add Exam

---
+++ exam/2task/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat a = new Cat("Dina", "Les");
+            Dog b = new Dog("Tasa", "dog");
+            SearchInfo.Func(a, b);
+        }
+    }
+}
